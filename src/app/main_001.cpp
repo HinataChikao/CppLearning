3b32e18a9cd1bf7f927437a573213173caf19ba7
= <=>
2022-12-19 20:22:06 +0330

    after creating main_001.cpp source file.

---
+++ src/app/main_001.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+	std::string name{};
+
+	std::cout "enter your name: ";
+	std::cin >> name;
+	std::cout << "your name is: " << name << std::endl;
+
+
+	std::cout << std::endl << "the end ..." << std::endl;
+	return EXIT_SUCCESS;
+}
